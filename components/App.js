de98308e6bf62e74aa2c3425ad13413318b62ada
Tony Tong <tony@theleagueapp.co>
2018-10-23 15:45:42 +0800

    Fixed your function.

--- components/App.js
+++ components/App.js
@@ -23,7 +23,8 @@ export default {
     };
   },
   methods: {
-    updateCurrentChart: function(newChart) {}
+    updateCurrentChart: function(newChart) {},
+    change: function (x) { }
   },
   firebase: {
     charts: {
